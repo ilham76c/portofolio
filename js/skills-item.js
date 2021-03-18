030b36b8f88cb641c24b59a1b7b55816d19b29ff
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-03-18 22:28:05 +0700

    Feature: me-render item di tab Skills menggunakan javascript

---
+++ js/skills-item.js
@@ -0,0 +1,47 @@
+const skills = [
+  'Laravel',
+  'Codeigniter',
+  'Express.js',
+  'Node.js',
+  'JQuery',
+  'Bootstrap',
+  'PHP',
+  'Javascript',
+  'HTML',
+  'CSS',
+  'MySQL',
+  'PostgreSQL',
+  'Redis',
+  'Redux',
+  'Docker'
+];
+
+class SkillsItem extends HTMLElement {
+
+  createSkillElement(skill) {
+    return `
+      <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+        <div class="card nav-bg">
+          <div class="row no-gutters">
+            <div class="col-3 col-xs-3">
+              <img class="img-thumbnail" src="../img/skills/${skill.toLowerCase()}64.png" alt="gambar">
+            </div>
+            <div class="col-9 col-xs-9 d-flex flex-fill align-items-center">
+              <div class="card-body">
+                <p class="card-text m-0">${skill}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  connectedCallback() {
+    skills.forEach((skill) => {
+      this.innerHTML += this.createSkillElement(skill);
+    });
+  }
+}
+
+customElements.define('skills-item', SkillsItem);
